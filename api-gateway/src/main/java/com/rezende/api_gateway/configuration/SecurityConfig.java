53ee9c48af108a55d329a579fcc6860b7f0c4550
Eduardo Rezende <dudurezende426@gmail.com>
2025-08-21 17:10:42 -0400

    Configure infrastructure and security with Keycloak and Gateway

---
+++ api-gateway/src/main/java/com/rezende/api_gateway/configuration/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.rezende.api_gateway.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(final ServerHttpSecurity httpSecurity) {
+
+        return httpSecurity
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("api/auth/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
---
+++ api-gateway/src/main/java/com/rezende/api_gateway/configuration/TestSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.rezende.api_gateway.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+@Profile("test")
+public class TestSecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain testSecurityFilterChain(final ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().permitAll()
+                )
+                .build();
+    }
+}
+
