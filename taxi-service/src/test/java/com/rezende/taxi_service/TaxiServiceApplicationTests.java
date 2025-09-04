ca7f5ad9f1f998b6054952a8ed485cf28458a5a8
Eduardo Rezende <dudurezende426@gmail.com>
2025-09-04 14:33:05 -0400

    taxi-service created

---
+++ taxi-service/src/test/java/com/rezende/taxi_service/TaxiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rezende.taxi_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
